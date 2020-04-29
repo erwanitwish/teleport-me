79a10f0a3067cb9c7738c25ab9524ed87e4e417c
erwanitwish <quillec.erwan@gmail.com>
2020-04-29 15:35:42 +0200

    un MAXIIIMUUUUUMMMMM d'algo

--- src/components/met-access.jsx
+++ src/components/met-access.jsx
@@ -8,46 +8,82 @@ class MetAccess extends React.Component {
       artId: "",
       art: "",
       loading: true,
+      letters: [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+      ],
+      letter: "",
+      arrayId: [],
     };
     this.getArt = this.getArt.bind(this);
-    this.getID = this.getID.bind(this);
+    this.getArrayID = this.getArrayID.bind(this);
+    this.randomLetter = this.randomLetter.bind(this);
   }
 
-  getID() {
-    const randomNumber = Math.floor(Math.random() * 50000) + 1;
-    console.log(randomNumber);
+  randomLetter() {
+    this.setState({
+      letter: this.state.letters[
+        Math.floor(Math.random() * (this.state.letters.length - 1))
+      ],
+    });
+  }
+
+  getArrayID() {
+    this.setState({ arrayID: [] });
     axios
       .get(
-        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomNumber}`
+        `https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&q=${this.state.letter}`
       )
       .then((res) => {
-        if (res.data.isHighlight === true) {
-          this.setState({ artId: res.data.objectID });
-          console.log(res.data);
-        } else {
-          this.getID();
-        }
+        this.setState({
+          arrayId: [...this.state.arrayId, ...res.data.objectIDs],
+        });
       })
-      .catch((err) => this.getID());
+      .catch((err) => this.getArrayID());
+    this.randomLetter();
   }
 
   componentDidMount() {
-    this.getID();
-  }
-
-  componentDidUpdate() {
-    this.getID();
+    this.randomLetter();
+    this.getArrayID();
   }
 
   getArt() {
+    const finalId = this.state.arrayId[
+      Math.floor(Math.random() * (this.state.arrayId.length - 1))
+    ];
     axios
       .get(
-        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${this.state.artId}`
+        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${finalId}`
       )
       .then((res) => {
         this.setState({ loading: false });
         this.setState({ art: res.data });
       });
+    this.getArrayID();
   }
 
   render() {
