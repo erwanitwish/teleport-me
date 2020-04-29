fef1507b1df0f5151e0c36f193a149bb6fcb55e9
erwanitwish <quillec.erwan@gmail.com>
2020-04-29 23:39:46 +0200

    ajout mineurs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import MetAccess from "./components/met-access";
 import WebcamAccess from "./components/webcam-access";
+import Credits from "./components/credits";
 
 function App() {
   return (
-    <div className="App">
-      <MetAccess />
-      <div className="background"></div>
-
-      <div className="behind">
-        <WebcamAccess />
-      </div>
-    </div>
+    <Router>
+      <switch>
+        <div className="App">
+          <Route exact path="/">
+            <MetAccess />
+            <div className="background"></div>
+            <div className="behind">
+              <WebcamAccess />
+            </div>
+          </Route>
+          <Route exact path="/credits">
+            <Credits />
+          </Route>
+        </div>
+      </switch>
+    </Router>
   );
 }
 
--- src/components/credits.jsx
+++ src/components/credits.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Credits() {
   return (
-    <div>
-      <h1>Design and Dev by Brice Le Gallo & Erwan Quillec</h1>
-      <p>thanks to Laura</p>
+    <div className="credits">
+      <Link to="/">
+        <button id="retour-button">Retour</button>
+      </Link>
+      <div className="text-credits">
+        <h1>
+          Design and Dev by <br />
+          Brice Le Gallo & Erwan Quillec
+        </h1>
+        <p>thanks to Laura</p>
+      </div>
     </div>
   );
 }
