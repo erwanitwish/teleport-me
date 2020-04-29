c4eb8d57d5ff822e4497c00a26cde97029a3afd5
erwanitwish <quillec.erwan@gmail.com>
2020-04-29 23:08:49 +0200

    credits

---
+++ src/components/credits.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Credits() {
+  return (
+    <div>
+      <h1>Design and Dev by Brice Le Gallo & Erwan Quillec</h1>
+      <p>thanks to Laura</p>
+    </div>
+  );
+}
+
+export default Credits;
