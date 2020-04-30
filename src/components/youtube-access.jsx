e672e4e0a8dff3de1cfdf7c13ad55a9bf33a22f4
erwanitwish <quillec.erwan@gmail.com>
2020-04-30 13:35:11 +0200

    presque tout

--- src/components/youtube-access.jsx
+++ src/components/youtube-access.jsx
@@ -1,53 +1,66 @@
 import React from "react";
 import axios from "axios";
 import ReactPlayer from "react-player";
-import { Spring, config } from 'react-spring/renderprops';
-import Button from './button';
+import { Spring, config } from "react-spring/renderprops";
+import Button from "./button";
 
 class YoutubeAccess extends React.Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {
-      youtubeUrl: 'https://www.youtube.com/watch?v=rgZfmWXTqas',
-      startTime: 50
+      youtubeUrl: "https://www.youtube.com/watch?v=rgZfmWXTqas",
+      startTime: Math.floor(Math.random() * 3500),
     };
-    this.handleRandomTime = this.handleRandomTime.bind(this);
+    // this.handleRandomTime = this.handleRandomTime.bind(this);
   }
 
-  handleRandomTime () {
-    this.setState({ startTime : Math.floor(Math.random() * 3500) });
-  }
+  // handleRandomTime() {
+  //   this.setState({ startTime: Math.floor(Math.random() * 3500) });
+  // }
 
-  componentDidMount() {
-    this.handleRandomTime();
-    console.log('Hello');
-  }
+  // componentDidMount() {
+  //   this.handleRandomTime();
+  //   console.log("Hello");
+  // }
 
-  render () {
+  render() {
     console.log(this.state.startTime);
     return (
       <>
-        {<Spring
-          config={config.slow}
-          delay={1000}
-          from={{ opacity: 0, transform: 'translateX(-10vw)' }}
-          to={{ opacity: 1, transform: 'translateX(0vw)' }}>
-          {props => <Button stylize={props} idButton='button2' func={this.handleRandomTime} butnTxt='TÉLÉPORTATION' move='left' />}
-        </Spring>}
+        {
+          // <Spring
+          //   config={config.slow}
+          //   delay={1000}
+          //   from={{ opacity: 0, transform: "translateX(-10vw)" }}
+          //   to={{ opacity: 1, transform: "translateX(0vw)" }}
+          // >
+          //   {(props) => (
+          //     <Button
+          //       stylize={props}
+          //       idButton="button2"
+          //       func={this.handleRandomTime}
+          //       butnTxt="TÉLÉPORTATION"
+          //       move="left"
+          //     />
+          //   )}
+          // </Spring>
+        }
         <div className="back-video">
           <ReactPlayer
             url={this.state.youtubeUrl}
-            width='100%'
-            height='100%'
-            playing
+            width="100%"
+            height="100%"
             muted
+            playing
             config={{
               youtube: {
                 playerVars: {
                   control: 0,
-                  start: this.state.startTime
-                }
-              }}} />
+                  start: this.state.startTime,
+                },
+              },
+            }}
+          />
         </div>
       </>
     );
