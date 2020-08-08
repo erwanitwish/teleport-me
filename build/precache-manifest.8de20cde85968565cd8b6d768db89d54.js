6daa9250e957cb5cc564d8597efb3c62ea9515f2
erwanitwish <quillec.erwan@gmail.com>
2020-08-08 14:54:37 +0200

    build added

---
+++ build/precache-manifest.8de20cde85968565cd8b6d768db89d54.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a87e14feb0e3a1041cbbf05910ef34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b9020c2558ae9276324",
+    "url": "/static/css/main.c77341b4.chunk.css"
+  },
+  {
+    "revision": "f6546c31acb1a81798a7",
+    "url": "/static/js/2.ab8c685f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab8c685f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9020c2558ae9276324",
+    "url": "/static/js/main.287dba73.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  },
+  {
+    "revision": "33ec1884a421d60307a10b8731dcebc6",
+    "url": "/static/media/background_6.33ec1884.gif"
+  },
+  {
+    "revision": "f4ffc6880d3ce9e451ac3d7ef34439cb",
+    "url": "/static/media/chevalet.f4ffc688.png"
+  },
+  {
+    "revision": "b52c77f1833e0a4b7b96f00a35fb19f5",
+    "url": "/static/media/credits.b52c77f1.jpg"
+  }
+]);
